e5ae2c0342df2b7618c268c4b1016632a6334717
Leonardo Margoti Meister <leozinhomm1@gmail.com>
2022-07-08 14:16:44 -0300

    Add project files.

---
+++ Poker.Dominio/Dominio/Carta.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poker.Dominio.Dominio
+{
+    public class Carta
+    {
+        public char _Naipe { get; set; }
+        public char _Carta { get; set; }
+
+        public int _ValorNaipe { get; set; }
+        public int _ValorCarta { get; set; }
+
+        public Carta(char naipe, char carta)
+        {
+            _Naipe = naipe;
+            _Carta = carta;
+
+            AlimentarValores();
+        }
+        
+        private void AlimentarValores()
+        {
+            _ValorCarta = DicionarioValores.Cartas[_Carta];
+            _ValorNaipe= DicionarioValores.Naipe[_Naipe];
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Carta carta &&
+                   _Naipe == carta._Naipe &&
+                   _Carta == carta._Carta &&
+                   _ValorNaipe == carta._ValorNaipe &&
+                   _ValorCarta == carta._ValorCarta;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(_Naipe, _Carta, _ValorNaipe, _ValorCarta);
+        }
+    }
+}
